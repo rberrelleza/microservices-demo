abb21e3ceaaac80ee0deb185724d77e2cf9cc99b
Ramiro Berrelleza <rberrelleza@gmail.com>
2021-11-05 14:40:04 -0700

    Add Okteto label
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- src/frontend/handlers.go
+++ src/frontend/handlers.go
@@ -112,7 +112,7 @@ func (plat *platformDetails) setPlatformDetails(env string) {
 		plat.provider = "Azure"
 		plat.css = "azure-platform"
 	} else {
-		plat.provider = "Dafiti + Okteto"
+		plat.provider = "Okteto"
 		plat.css = "gcp-platform"
 	}
 }
